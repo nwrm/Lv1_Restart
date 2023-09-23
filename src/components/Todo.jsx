fc3aebf48c051c8a68e837f6027fb94cf0191f03
NEWROUM <106915189+nwrm@users.noreply.github.com>
2023-09-23 15:19:23 +0900

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,7 @@
 //child component - Todo.jsx
 
+import React from "react";
+
 const Todo = ({
   item,
   clickRemoverButtonHandler,
@@ -10,7 +12,9 @@ const Todo = ({
       <h4>{item.title}</h4>
       <p>{item.content}</p>
       <button onClick={clickRemoverButtonHandler}>X</button>
-      <button onClick={clickUpdateButtonHandler}>완료</button>
+      <button onClick={clickUpdateButtonHandler}>
+        {item.isDone ? "취소" : "완료"}  // if로 변경시 -> let buttonText; if (item.isDone) {buttonText = "취소";} else {buttonText = "완료";}
+      </button>
     </div>
   );
 };
